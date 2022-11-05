e75638c34190ca18d00a67b524fef9571179db66
Sasha M <5541alex5541@gmail.com>
2022-11-05 14:24:06 +0700

    Global code remaking + (add logo and menu)

--- main.c
+++ main.c
@@ -1,37 +1,113 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+void gameLogo()
+{
+    printf("**    *  ***  **   **      ****      *     **   **  *****\n");
+    printf("* *   *   *   * * * *     **  *     * *    * * * *  *    \n");
+    printf("*   * *   *   *  *  *     *    *   *****   *  *  *  ***  \n");
+    printf("*    **   *   *     *      ****   *     *  *     *  *****\n\n");
+}
+
+void menu(char menuInput)
+{
+    printf("#########################################################\n");
+    printf("Добро пожаловать!\n");
+    printf("1 - Выбрать режим игры\n");
+    printf("2 - Правила\n");
+    printf("3 - Выход\n");
+    printf("Введите: ");
+    scanf("%c", &menuInput);
+    printf("\n");
+
+    if(menuInput == '1'){
+        gameModeChoose();
+        play();
+    }
+    else if(menuInput == '2'){
+        printf("Правила: имеется три кучи фишек. В свой ход игрок может взять любое кол-во фишек(кроме 0)\nиз любой кучи. Побеждает тот, кто возьмёт последние фишки!\n\n");
+        _Exit(EXIT_SUCCESS);
+    }
+    else if(menuInput == '3'){
+        _Exit(EXIT_SUCCESS);
+    }
+    else{
+        printf("Некорректный ввод!\n");
+        _Exit(EXIT_SUCCESS);
+    }
+}
+
+int gameModeChoose()
+{
+    int gameMode = 0, mode = 0;
+
+    printf("1 - Игра вдвоём\n");
+    printf("2 - Игра против ИИ\n");
+    printf("Введите: ");
+    scanf("%d", &gameMode);
+    printf("\n");
+    if(gameMode == 1){
+        mode = 1;
+    }
+    else if(gameMode == 2){
+        mode = 2;
+    }
+    else{
+        printf("Некорректный режим!\n");
+        gameModeChoose();
+    }
+
+    return mode;
+}
+
+int play()
 {
     int game = 1;
     int heap = 0, take = 0;
     int heap1 = 3, heap2 = 4, heap3 = 5;
     int counter = 1;
 
-    //start and rules
-    printf("Игра 'Ним'\nПравила: имеется три кучи фишек. В свой ход игрок может взять любое кол-во фишек(кроме 0)\nиз любой кучи. Побеждает тот, кто возьмёт последние фишки!\n");
-
     while(game != 0)
     {
-        int ladno = 0;
+        int inGame = 0;
         if(counter % 2 == 1){
-            printf("Ходит первый игрок: ");
+            printf("Ходит первый игрок...");
         }
         if(counter % 2 == 0){
-            printf("Ходит второй игрок: ");
+            printf("Ходит второй игрок...");
         }
-        printf("%d %d %d\n", heap1, heap2, heap3);
-        printf("Введите номер кучи и количество фишек для взятия: ");
-        scanf("%d %d", &heap, &take);
+        printf("\n");
+
+        printf("(%d) ", heap1);
+        for(int i = 0; i < heap1; i++){
+            printf("*");
+        }
+        printf("\n");
+        printf("(%d) ", heap2);
+        for(int i = 0; i < heap2; i++){
+            printf("*");
+        }
+        printf("\n");
+        printf("(%d) ", heap3);
+        for(int i = 0; i < heap3; i++){
+            printf("*");
+        }
+        printf("\n");
+
+        printf("Введите номер кучи: ");
+        scanf("%d", &heap);
+        printf("Введите количество фишек для взятия: ");
+        scanf("%d", &take);
+        printf("\n");
 
         //cant take from empty heap
-        while(ladno == 0){
+        while(inGame == 0){
             if((heap == 1 && heap1 == 0) || (heap == 2 && heap2 == 0) || (heap == 3 && heap3 == 0)){
                 printf("Куча пуста!\n");
                 scanf("%d %d", &heap, &take);
             }
             else{
-                ladno = 1;
+                inGame = 1;
             }
         }
 
@@ -79,6 +155,16 @@ int main()
         
         counter++;
     }
+    printf("Старт игры\n");
+    return 0;
+}
+
+int main()
+{
+    char menuInput;
+
+    gameLogo();
+    menu(menuInput);
 
     return 0;
 }
