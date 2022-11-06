0bf7e0a703b5d947d6220eb183f05b51a50da8c7
Sasha M <5541alex5541@gmail.com>
2022-11-07 04:31:18 +0700

    Add bot mode

--- main.c
+++ main.c
@@ -6,13 +6,16 @@ void gameLogo()
     printf("**    *  ***  **   **      ****      *     **   **  *****\n");
     printf("* *   *   *   * * * *     **  *     * *    * * * *  *    \n");
     printf("*   * *   *   *  *  *     *    *   *****   *  *  *  ***  \n");
-    printf("*    **   *   *     *      ****   *     *  *     *  *****\n\n");
+    printf("*    **  ***  *     *      ****   *     *  *     *  *****\n\n");
 }
 
 void menu(char menuInput)
 {
+    menuInput = '1';
+
     printf("#########################################################\n");
     printf("Добро пожаловать!\n");
+    printf("#########################################################\n");
     printf("1 - Выбрать режим игры\n");
     printf("2 - Правила\n");
     printf("3 - Выход\n");
@@ -21,7 +24,6 @@ void menu(char menuInput)
     printf("\n");
 
     if(menuInput == '1'){
-        gameModeChoose();
         play();
     }
     else if(menuInput == '2'){
@@ -62,100 +64,261 @@ int gameModeChoose()
 
 int play()
 {
-    int game = 1;
+    int game = 1, good = 0;
     int heap = 0, take = 0;
     int heap1 = 3, heap2 = 4, heap3 = 5;
     int counter = 1;
+    int mode = gameModeChoose();
 
-    while(game != 0)
-    {
-        int inGame = 0;
-        if(counter % 2 == 1){
-            printf("Ходит первый игрок...");
-        }
-        if(counter % 2 == 0){
-            printf("Ходит второй игрок...");
-        }
-        printf("\n");
-
-        printf("(%d) ", heap1);
-        for(int i = 0; i < heap1; i++){
-            printf("*");
-        }
-        printf("\n");
-        printf("(%d) ", heap2);
-        for(int i = 0; i < heap2; i++){
-            printf("*");
-        }
-        printf("\n");
-        printf("(%d) ", heap3);
-        for(int i = 0; i < heap3; i++){
-            printf("*");
-        }
-        printf("\n");
-
-        printf("Введите номер кучи: ");
-        scanf("%d", &heap);
-        printf("Введите количество фишек для взятия: ");
-        scanf("%d", &take);
-        printf("\n");
-
-        //cant take from empty heap
-        while(inGame == 0){
-            if((heap == 1 && heap1 == 0) || (heap == 2 && heap2 == 0) || (heap == 3 && heap3 == 0)){
-                printf("Куча пуста!\n");
-                scanf("%d %d", &heap, &take);
-            }
-            else{
-                inGame = 1;
-            }
-        }
-
-        //checking from which heap to take
-        if(heap == 1){
-            if(heap1 != 0){
-                heap1 -= take;
-            }
-        }
-        else if(heap == 2){
-            if(heap2 != 0){
-                heap2 -= take;
-            }
-        }
-        else if(heap == 3){
-            if(heap3 != 0){
-                heap3 -= take;
-            }
-        }
-        else{
-            printf("неправильно\n");
-        }
-
-        //checking for a negative number of elements
-        if(heap1 < 0){
-            heap1 = 0;
-        }
-        if(heap2 < 0){
-            heap2 = 0;
-        }
-        if(heap3 < 0){
-            heap3 = 0;
-        }
-
-        //victory conditions
-        if(heap1 == 0 && heap2 == 0 && heap3 == 0){
-            game = 0;
+    if(mode == 1){
+        while(game != 0)
+        {
+            int inGame = 0;
             if(counter % 2 == 1){
-                printf("Фишки кончились.\nПобедил первый игрок!\n");
+                printf("Ходит первый игрок...");
             }
             if(counter % 2 == 0){
-                printf("Фишки кончились.\nПобедил второй игрок!\n");
+                printf("Ходит второй игрок...");
             }
+            printf("\n");
+
+            printf("(%d) ", heap1);
+            for(int i = 0; i < heap1; i++){
+                printf("*");
+            }
+            printf("\n");
+            printf("(%d) ", heap2);
+            for(int i = 0; i < heap2; i++){
+                printf("*");
+            }
+            printf("\n");
+            printf("(%d) ", heap3);
+            for(int i = 0; i < heap3; i++){
+                printf("*");
+            }
+            printf("\n");
+
+            printf("Введите номер кучи: ");
+            scanf("%d", &heap);
+            printf("Введите количество фишек для взятия: ");
+            scanf("%d", &take);
+            printf("\n");
+
+            //cant take from empty heap
+            while(inGame == 0){
+                if((heap == 1 && heap1 == 0) || (heap == 2 && heap2 == 0) || (heap == 3 && heap3 == 0)){
+                    printf("Куча пуста!\n");
+                    printf("Введите номер кучи: ");
+                    scanf("%d", &heap);
+                    printf("Введите количество фишек для взятия: ");
+                    scanf("%d", &take);
+                }
+                else{
+                    inGame = 1;
+                }
+            }
+
+            //checking from which heap to take
+            if(heap == 1){
+                if(heap1 != 0){
+                    heap1 -= take;
+                }
+            }
+            else if(heap == 2){
+                if(heap2 != 0){
+                    heap2 -= take;
+                }
+            }
+            else if(heap == 3){
+                if(heap3 != 0){
+                    heap3 -= take;
+                }
+            }
+            else{
+                printf("Ошибка! Выберите существующую кучу!\n\n");
+            }
+
+            //checking for a negative number of elements
+            if(heap1 < 0){
+                heap1 = 0;
+            }
+            if(heap2 < 0){
+                heap2 = 0;
+            }
+            if(heap3 < 0){
+                heap3 = 0;
+            }
+
+            //victory conditions
+            if(heap1 == 0 && heap2 == 0 && heap3 == 0){
+                game = 0;
+                if(counter % 2 == 1){
+                    printf("\nФишки кончились.\nПобедил первый игрок!\n");
+                }
+                if(counter % 2 == 0){
+                    printf("\nФишки кончились.\nПобедил второй игрок!\n");
+                }
+            }
+            
+            counter++;
         }
-        
-        counter++;
     }
-    printf("Старт игры\n");
+    else if(mode == 2){
+        while(game != 0){
+            int inGame = 0;
+            if(counter % 2 == 1){
+                printf("Ходит игрок...");
+            }
+            if(counter % 2 == 0){
+                printf("Ходит компьютер...");
+            }
+            printf("\n");
+
+            printf("(%d) ", heap1);
+            for(int i = 0; i < heap1; i++){
+                printf("*");
+            }
+            printf("\n");
+            printf("(%d) ", heap2);
+            for(int i = 0; i < heap2; i++){
+                printf("*");
+            }
+            printf("\n");
+            printf("(%d) ", heap3);
+            for(int i = 0; i < heap3; i++){
+                printf("*");
+            }
+            printf("\n");
+
+            //player turn
+            if(counter % 2 != 0){
+                printf("Введите номер кучи: ");
+                scanf("%d", &heap);
+                printf("Введите количество фишек для взятия: ");
+                scanf("%d", &take);
+                printf("\n");
+
+                if(heap == 1){
+                    if(heap1 != 0){
+                        heap1 -= take;
+                    }
+                }
+                else if(heap == 2){
+                    if(heap2 != 0){
+                        heap2 -= take;
+                    }
+                }
+                else if(heap == 3){
+                    if(heap3 != 0){
+                        heap3 -= take;
+                    }
+                }
+                else{
+                    printf("Ошибка! Выберите существующую кучу!\n\n");
+                }
+            }
+            //bot turn
+            else{
+                if(heap1 > 1 && heap2 > 1 && heap3 > 1){
+                    take = heap1 - 1;
+                    heap1 -= take;
+                    heap = 1;
+                }
+                else if(heap1 == 1 && heap2 > 1 && heap3 > 1){
+                    take = heap2 - 1;
+                    heap2 -= take;
+                    heap = 2;
+                }
+                else if(heap1 == 1 && heap2 == 1 && heap3 > 1){
+                    take = heap3;
+                    heap3 -= take;
+                    heap = 3;
+                }
+                else if(heap1 == 1 && heap2 == 0 && heap3 == 0){
+                    take = heap1;
+                    heap1 -= take;
+                    heap = 1;
+                }
+                else if(heap2 == 1 && heap1 == 0 && heap3 == 0){
+                    take = heap2;
+                    heap2 -= take;
+                    heap = 2;
+                }
+                else if(heap1 == 0 && heap2 == 0 && heap3 != 0){
+                    take = heap3;
+                    heap3 -= take;
+                    heap = 3;
+                }
+                else if(heap1 != 0 && heap2 == 0 && heap3 == 0){
+                    take = heap1;
+                    heap1 -= take;
+                    heap = 1;
+                }
+                else if(heap1 == 0 && heap2 != 0 && heap3 == 0){
+                    take = heap2;
+                    heap2 -= take;
+                    heap = 2;
+                }
+                else{
+                    good = 0;
+                    while(good != 1)
+                    {          
+                        heap = rand() % 4;
+                        if(heap == 1 && heap1 == 0){
+                            heap = rand() % 4;
+                        }
+                        else if(heap == 2 && heap2 == 0){
+                            heap = rand() % 4;
+                        }
+                        else if(heap == 3 && heap3 == 0){
+                            heap = rand() % 4;
+                        }
+                        else{
+                            good = 1;
+                            if(heap == 1){
+                                take = rand() % heap1 + 1;
+                                heap1 -= take;
+                            }
+                            else if(heap == 2){
+                                take = rand() % heap2 + 1;
+                                heap2 -= take;
+                            }
+                            else if(heap == 3){
+                                take = rand() % heap3 + 1;
+                                heap3 -= take;
+                            }
+                        }
+                    }
+                }
+                printf("Компьютер взял %d фишек из %d кучи.\n\n", take, heap);
+            }
+
+            //checking for a negative number of elements
+            if(heap1 < 0){
+                heap1 = 0;
+            }
+            if(heap2 < 0){
+                heap2 = 0;
+            }
+            if(heap3 < 0){
+                heap3 = 0;
+            }
+
+            //victory conditions
+            if(heap1 == 0 && heap2 == 0 && heap3 == 0){
+                game = 0;
+                if(counter % 2 == 1){
+                    printf("\nФишки кончились.\nПобедил игрок!\n");
+                }
+                if(counter % 2 == 0){
+                    printf("\nФишки кончились.\nПобедил компьютер!\n");
+                }
+            }
+        
+            counter++;
+        }
+    }
+
     return 0;
 }
 
